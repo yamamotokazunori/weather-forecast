82e574e5312cce7e8e3a1931716e5cfd90954b38
kazunori <gaogaigar673@gmail.com>
2021-04-23 17:03:49 +0900

    [Finish]js 12章

---
+++ js/script.js
@@ -0,0 +1,21 @@
+API_KEY = "1ca54553f54b08ca41d3597f64071b4b";
+
+$(function(){
+  $('#btn').on('click', function(){
+    $.ajax({
+      url: "http://api.openweathermap.org/data/2.5/weather?q=" + $('#cityname').val() + "&units=metric&appid=" + API_KEY,
+      dataType : 'jsonp',
+    }).done(function (data){
+      $('#place').text(data.name);
+      $('#temp_max').text(data.main.temp_max);
+      $('#temp_min').text(data.main.temp_min);
+      $('#humidity').text(data.main.humidity);
+      $('#speed').text(data.wind.speed);
+      $('#weather').text(data.weather[0].main);
+      $('img').attr("src","http://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
+      $('img').attr("alt",data.weather[0].main);
+    }).fail(function (data){
+      alert('通信に失敗しました。')
+    });
+  });
+});
